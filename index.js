1254a23c76b1130f8cf5909746b8e6f01e51dc21
VANTEROUS <70611715+VANTEROUS@users.noreply.github.com>
2025-04-21 01:12:03 -0500

    Typo Correction

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const quoteGenerateButton = document.querySelector('.quote-generator__link');
 
 quoteGenerateButton.addEventListener('click', async() => {
     try {
-        const response = await fetch('https://joke-generator-api-project.onrender.com');
+        const response = await fetch('https://joke-generator-api-project.onrender.com/random');
         console.log(JSON.stringify(response))
         const data = await response.json();
         quoteText.innerHTML = `${data.setup} <br><br> ${data.punchline}`;
